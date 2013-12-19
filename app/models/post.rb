e7d3bac98e1082f314860befff8303a894b8e972
takuro yoshimi <hikago_uining@hotmail.com>
2013-12-19 15:32:24 +0900

    paperclip test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
-  has_attached_file :avatar, styles: { :thumb => ["50x50#", :jpg], :medium => ["100x100#", :jpg], :large => ["300x300>", :jpg] }, default_url: "http://nikkegroup.com/wp-content/uploads/2013/08/nikke-top1.jpg"
+  has_attached_file :avatar, styles: { :thumb => ["50x50#", :jpg], :medium => ["100x100#", :jpg], :large => ["300x300>", :jpg] },
+  default_url: "http://nikkegroup.com/wp-content/uploads/2013/08/nikke-top1.jpg",
+  :url => "/system/:attachment/:id/:style/:basename.:extension",
+  :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
 
   has_many :comments
 
